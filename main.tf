46e7686f677a0213fe4d81696460395392c64e7c
Jamal Ahmed <jamal.ahmed@cmrsurgical.com>
2023-05-15 22:05:39 +0100

    changes to the cidr

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "jamal-vpc"
-  cidr = "10.10.0.0/16"
+  cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
