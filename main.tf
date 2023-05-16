d24d2ccf5fb9cd73365dec8e4bff2912cedc8c01
Jamal Ahmed <jamal.ahmed@cmrsurgical.com>
2023-05-16 18:26:20 +0100

    added var

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "jamal-server" {
   ami           = "ami-09fd16644beea3565"
-  instance_type = instance_type
+  instance_type = var.instance_type
 
   tags = {
     Name = "jamal-server"
