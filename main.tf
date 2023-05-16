417f9fb5637bd4de3afd4d49500612b218ab56af
Jamal Ahmed <jamal.ahmed@cmrsurgical.com>
2023-05-16 18:12:24 +0100

    provisioners

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "jamal-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_instance" "jamal-server" {
+  ami           = "ami-09fd16644beea3565"
+  instance_type = var.instance_type
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "jamal-server"
   }
+
 }
