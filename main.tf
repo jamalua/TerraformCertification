aa6b842692ea6a936a62576cb8af7a7682493b9b
Jamal Ahmed <jamal.ahmed@cmrsurgical.com>
2023-05-19 21:48:51 +0100

    changed the key-pair name

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_key_pair" "deployer" {
 resource "aws_instance" "jamal_server" {
   ami                    = "ami-09fd16644beea3565"
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
+  key_name               = "jamal-test-keypair"
   vpc_security_group_ids = [aws_security_group.sg_jamal_server.id]
   user_data              = data.template_file.user_data.rendered
   tags = {
