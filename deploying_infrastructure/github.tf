49fac412f9d616f799f260ce2948f1f2666f318b
Jamal Ahmed <jamal.ahmed@cmrsurgical.com>
2023-06-20 16:38:30 +0100

    commit

---
+++ deploying_infrastructure/github.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.28.0"
+    }
+  }
+}
+
+provider "github" {
+  token = "github_pat_AG4H25I0zAFdo5wZOym1_IJems8gHoGbRAZx6eQ16mMvtzCt3FxVNMxoI4GAGOjXMFA4I7QHbHYwBbBQ"
+}
+
+resource "github_repository" "example" {
+  name        = "terrafrom-created-repo"
+  description = "My awesome codebase"
+
+  visibility = "public"
+
+}
